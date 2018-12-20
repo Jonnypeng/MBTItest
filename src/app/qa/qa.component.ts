1621a932f2e4e09958dbd119116efd7e82fd8362
jonny <jonny.peng@qq.com>
2018-12-20 11:15:54 +0800

    sync

--- src/app/qa/qa.component.ts
+++ src/app/qa/qa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit ,Input} from '@angular/core';
+import { qas } from '../question';
 
 @Component({
   selector: 'app-qa',
@@ -8,13 +9,20 @@ import { Component, OnInit ,Input} from '@angular/core';
 
 
 export class QaComponent implements OnInit {
+  id:number = 0;
+  qaes = qas;
 
   constructor() { 
     
   }
 
   ngOnInit() {
+      console.log(qas);
     
   }
+
+
+
+
 
 }
